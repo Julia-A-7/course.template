d248bafe294e940996fc5ab3b842af493eddeeaf
Julia-A-7 <you@example.com>
2023-12-01 10:55:11 +0700

    transactional

--- backend/src/main/java/ru/javaschool/documents/repository/DocumentRepository.java
+++ backend/src/main/java/ru/javaschool/documents/repository/DocumentRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.Query;
 import ru.javaschool.documents.repository.entity.DocumentsEntity;
 import ru.javaschool.documents.repository.entity.StatusEntity;
 
+import javax.transaction.Transactional;
 import java.util.Optional;
 
 public interface DocumentRepository extends JpaRepository<DocumentsEntity, Long> {
@@ -16,6 +17,7 @@ public interface DocumentRepository extends JpaRepository<DocumentsEntity, Long>
      * @param status статус документа
      * @param id     код документа
      */
+    @Transactional
     @Modifying
     @Query("update DocumentsEntity d set d.status = ?1 where d.id = ?2")
     void updateStatusById(@Nullable StatusEntity status, Long id);
